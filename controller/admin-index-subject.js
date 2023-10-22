47b62ab0f5f81d60bfcb5903e8fb341167b0e23a
illuminxry <secret.ian0926@gmail.com>
2023-10-22 22:01:28 +0800

    created a view function for the subject

--- controller/admin-index-subject.js
+++ controller/admin-index-subject.js
@@ -11,7 +11,7 @@ exports.getSubjectIndexPage = (req, res) => {
     const sql = `
         SELECT s.subjectid, s.subjectname, s.sectionname, s.teacherid, td.firstname, td.middlename, td.lastname
         FROM subjects as s
-        INNER JOIN teacherdetails as td ON s.teacherid = td.id
+        INNER JOIN teacherdetails as td ON s.teacherid = td.id where visibility = 'Visible'
     `;
 
     const connection = mysql.createConnection(conn);

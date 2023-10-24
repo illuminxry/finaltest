7c455c6bd106b779784c83006d139f961a26a8e7
illuminxry <secret.ian0926@gmail.com>
2023-10-24 19:09:50 +0800

    create section not working

--- routes/admin-login.js
+++ routes/admin-login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const adminLogin = require('../controller/admin-login'); // Import the controller
 /* GET admin-login page. */
-router.get('/admin/login',adminLogin.getAdminLogin);
+router.get('/admin/',adminLogin.getAdminLogin);
 router.post('/admin/login',adminLogin.postAdminLogin);
 
 module.exports = router;

0aa865b6bd035b40bd63e95d6ac4e05a80167afa
illuminxry <secret.ian0926@gmail.com>
2023-10-25 20:45:57 +0800

    edit section, now working

--- routes/admin-edit-section.js
+++ routes/admin-edit-section.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const adminEditSection = require('../controller/admin-edit-section'); // Import the controller
 /* GET admin-edit-section page. */
 router.get('/admin/edit-section/:id',adminEditSection.getEditSectionPage);
-// router.post('/admin/edit-subject/:id', adminEditSubject.postEditSubjectPage);
+router.post('/admin/edit-section/:id', adminEditSection.postEditSectionPage);
 
 
 module.exports = router;
